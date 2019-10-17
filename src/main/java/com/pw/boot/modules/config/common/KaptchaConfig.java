8f8b9178787cfa549f5aa5103cb8b97d91268145
jiacanh <hongjiacan@foxmai.com>
2019-10-17 15:43:09 +0800

    '1017'

---
+++ src/main/java/com/pw/boot/modules/config/common/KaptchaConfig.java
@@ -0,0 +1,9 @@
+package com.pw.boot.modules.config.common;
+
+/**
+ * @description:
+ * @author: hjc
+ * @create: 2019-10-17
+ */
+public class KaptchaConfig {
+}
---
+++ src/test/java/com/pw/boot/sys/SysInitializeTest.java
@@ -0,0 +1,9 @@
+package com.pw.boot.sys;
+
+/**
+ * @description:
+ * @author: hjc
+ * @create: 2019-09-16
+ */
+public class SysInitializeTest {
+}
